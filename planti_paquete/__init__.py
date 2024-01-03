d48847b48033daa224e2c154d8a49a7200ac7ef0
Cristina <cristinaduarte88@gmail.com>
2024-01-03 15:37:21 -0300

    cambié el nombre del autor

--- planti_paquete/__init__.py
+++ planti_paquete/__init__.py
@@ -1,5 +1,5 @@
 __version__ = "0.1.0"
-__author__ = 'Batman, Mujer Maravilla, Tito'
+__author__ = 'Cristina Daiana Duarte'
 __credits__ = 'UNS - CONICET'
 
 # importen los paquetes necesarios para que las funciones funcionen
